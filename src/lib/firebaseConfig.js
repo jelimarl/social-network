ae48656f7d2b74902c31c910ab12f57214f57e0e
Amappola <anam.porrasp@gmail.com>
2022-09-02 11:59:06 -0500

    Subiendo módulo de configuración de firebas

---
+++ src/lib/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCUqP6Q6YWINXFArtQywALs6lWV7SvT8IU",
+  authDomain: "system-laboratoria.firebaseapp.com",
+  projectId: "system-laboratoria",
+  storageBucket: "system-laboratoria.appspot.com",
+  messagingSenderId: "965084672335",
+  appId: "1:965084672335:web:cbac7d7664cc0748bad53a",
+  measurementId: "G-B4FV1DWMGE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
