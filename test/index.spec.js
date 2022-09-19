347389eeb8ded298483fea4c4752d70c6f13ff88
jelimarl <jelimarlm@gmail.com>
2022-09-19 08:49:08 -0500

    Test unitarios HU 1 Y 2

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,12 @@
 /* eslint-disable import/no-unresolved */
 // importamos la funcion que vamos a testear
-
+import { register } from '../src/components/register.js';
 import { showSection } from '../src/router.js';
+import { googleSignIn } from '../src/lib/firebaseServices.js';
 
-jest.mock('../src/lib/firebaseServices.js');
+jest.mock('../src/lib/firebaseServices.js', () => ({
+  googleSignIn: jest.fn(),
+}));
 
 describe('Router', () => {
   it('Tiene que llamar a la función login', () => {
@@ -28,5 +31,19 @@ describe('Router', () => {
     showSection();
 
     expect(document.querySelector('h2.register__title').textContent).toBe('Create account');
+  });
+});
+
+describe('register', () => {
+  it('Comprueba que funciona el evento click de Google', () => {
+    const view = register();
+
+    googleSignIn.mockImplementation(() => Promise.resolve('works'));
+
+    const googleButton = view.querySelector('.register__button-google');
+
+    googleButton.dispatchEvent(new Event('click'));
+
+    expect(googleSignIn).toBeCalled();
   });
 });
