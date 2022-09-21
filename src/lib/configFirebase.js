84861a2d130d2dcfbaf6412878fa4fb7c89a086f
Amappola <anam.porrasp@gmail.com>
2022-09-21 16:30:11 -0500

    Subiendo el archivo de configuración de firebase

---
+++ src/lib/configFirebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCUqP6Q6YWINXFArtQywALs6lWV7SvT8IU',
+  authDomain: 'system-laboratoria.firebaseapp.com',
+  projectId: 'system-laboratoria',
+  storageBucket: 'system-laboratoria.appspot.com',
+  messagingSenderId: '965084672335',
+  appId: '1:965084672335:web:cbac7d7664cc0748bad53a',
+  measurementId: 'G-B4FV1DWMGE',
+};
